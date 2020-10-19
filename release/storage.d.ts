558e9d027480af3d54ffa8d075dc2c4815a79de3
gu.jieyi(PUHUI-顾杰义) <gu.jieyi@msi.xizi.local>
2020-10-19 09:48:22 +0800

    fix: storage.d.ts save remove return without definition

--- release/storage.d.ts
+++ release/storage.d.ts
@@ -1,29 +1,24 @@
-
-
 export default Storage;
 
 export as namespace Storage;
 
-
 declare namespace Storage {
+    type Key = string;
+    type Value = string | number;
+    type Time = number;
 
-  type Key = string;
-  type Value = string | number;
-  type Time = number;
+    interface Session {
+        get: (key: Key) => string;
+        save: (key: Key, value: Value) => void;
+        remove: (key: Key, value: Value) => void;
+        clear: () => void;
+    }
 
-  interface Session {
-    get: (key: Key) => string;
-    save: (key: Key, value: Value) => void;
-    remove: (key: Key, value: Value) => void;
-    clear: () => void;
-  }
-
-  function get(key: Key): string;
-  function save(key: Key, value: Value, time?: Time);
-  function remove(key: Key);
-  function clear(): void;
-  const session: Session;
-  function on(key: Key, fn: Function): void
-  function off(key: Key, fn?: Function): void;
+    function get(key: Key): string;
+    function save(key: Key, value: Value, time?: Time): void;
+    function remove(key: Key): void;
+    function clear(): void;
+    const session: Session;
+    function on(key: Key, fn: Function): void;
+    function off(key: Key, fn?: Function): void;
 }
-
